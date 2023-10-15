6028da6ba30a14dad3095e05854ad4762eb867e9
WeronikaGziut <01161716@pw.edu.pl>
2023-10-15 10:45:58 +0200

    Refactor

--- example.py
+++ example.py
@@ -1,12 +1,6 @@
 def sweap_max(items: list) -> list:
-    max_item = items[0]
-    item_zero = items[0]
-    for i in range(1, len(items)):
-        if items[i] > max_item:
-            max_item = items[i]
-            items[0] = items[i]
-            max_pos = i
-    items[max_pos] = item_zero
+    max_pos = items.index(max(items))
+    items[0], items[max_pos] = items[max_pos], items[0]
     return items
 
 print(sweap_max([2,8,10,0,5,7]))
