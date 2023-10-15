ca7ed5a8e094f44247d8f45c86dbbe9c08b8b710
WeronikaGziut <01161716@pw.edu.pl>
2023-10-15 14:26:17 +0200

    Added basic dictionaries example more

--- dict example.py
+++ dict example.py
@@ -1,15 +1,33 @@
-data = {}
+import json
+from pathlib import Path
+
+path = Path("data.json")
+
+if path.exists():
+    f = open(path, "r")
+    data = json.load(f)
+    f.close()
+else:
+    data = {}
+
+commands = ["new_contact", "show_contacts"]
 while True:
-    command = input("Wybierz opcje: new_contact (new), show_contacts (show): ")
-    if command == "new":
+    command = input(f"Wybierz opcje: {commands} ")
+    while command not in commands:
+        print("Unknown command, try again: ")
+        command = input(f"Wybierz opcje: {commands} ")
+    if command == "new_contact":
         a= input("wprowadz imie i nazwisko: ")
         b= input("wprowadz nr telefonu: ")
-        c= input("podaj wiek: ")
-        data[a] = [b, c]
-    elif command =="show":
+        data[a] = [b]
+    elif command =="show_contacts":
         print(data)
-    # else:
-        # print("unknow command")
-    command = input("Do you want to continue Y/N? ")
-    if command == "N":
+    answer = input("Do you want to continue Y/N? ")
+    while answer.lower() not in ["y", "n"]:
+        print("Choose Y or N only")
+        answer = input("Do you want to continue Y/N? ")
+    if answer.lower() == "n":
+        f = open("data.json", "w")
+        json.dump(data, f)
+        f.close()
         break
