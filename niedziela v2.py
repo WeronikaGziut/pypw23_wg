4fcb47df5de64c0a0b4b4593dde1d864f41bc99f
WeronikaGziut <01161716@pw.edu.pl>
2023-10-22 15:41:25 +0200

    niedziela 21/10

--- niedziela v2.py
+++ niedziela v2.py
@@ -1,3 +1,31 @@
 a = []
 
-a.append(42)
+a.append(42) #append to tak naprawde metoda klasy
+
+class Car:
+#self to argument ktory zawsze jest, pierwszy, tylko w deklaracji metody
+#to wskaznik na obiekt klasy/instancje
+    def __init__(self, registration_number): #konstruktor, poczatkowe ustawienia
+        self.registration_number = registration_number
+        self.speed = 0
+
+    def drive(self):
+        self.speed += 10
+
+    def __repr__(self):
+        return f"Car ({self.registration_number})"
+
+car = Car("WAW-17236") #odpalamy init
+car.drive()
+print(car)
+
+
+
+
+
+
+
+
+
+
+
